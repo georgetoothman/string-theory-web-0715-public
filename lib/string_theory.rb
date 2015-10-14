c18e4410957402e073e15e70d810411c1f46bc7f
George Toothman <george.toothman@flatironschool.com>
2015-10-14 09:37:48 -0400

    add oo solution

--- lib/string_theory.rb
+++ lib/string_theory.rb
@@ -1,3 +1,15 @@
-def sort_by_appearance(characters_to_sort, long_text)
-  # code goes here
+require 'pry'
+class Test
+  def sort_by_appearance(characters_to_sort, long_text)
+    count_hash = Hash.new
+    characters_to_sort.each_char do |char|
+      char_count = long_text.count(char)
+      count_hash[char] = char_count
+    end
+    sorted_count = count_hash.values.sort!
+    puts count_hash.keys.reverse.join
+  end
 end
+
+test = Test.new
+test.sort_by_appearance("$&*", "Lo**remips&umdolo*sesdf*rsita$$met")
